125325d6b2dcde31392b792d7bb6cfca2f757ebf
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-28 03:40:43 +0330

    update

--- main.py
+++ main.py
@@ -4,43 +4,47 @@ import time
 
 from sms_boomber import sms_boomber
 from colorama import Fore as c
-from setting import (
-    INPUT_STRUCT
-)
-class main:
-    commands_help = ['-h','-help']
-    def lunch():
-        def help():
-            print('''Commands\t\t\tUsage\n\nclear\t\t\tfor clear terminal\nexit\t\t\tfor exit script\nstart\t\t\tTo start the script and give information\n''')
 
-        while True:
-            terminal = input(" $ ")
 
-            if terminal in commands_help:
-                help()
-            elif == 'clear'
-                os.system('clear' or 'cls')
-            elif  == :
-        print(c.CYAN + "Please enter your target phonenumber")
-        phonenumber = input(c.WHITE + " $ ")
-
-        print(c.CYAN + "Please enter sms number")
-        sms_number = input(c.WHITE + " $ ")
-
-        sms_boomber(phonenumber, sms_number)
-
-    def banner():
+def banner():
         os.system("clear" or "cls")
 
         print(c.GREEN + """
    ___ ___  ___ ___ _    ___ __ _  ___
   (_-</ _ \/ _ `/  ' \  (_-</  ' \(_-< Help: -h,-help
- /___/ .__/\_,_/_/_/_/ /___/_/_/_/___/"""+c.WHITE+"""V 1.6 Rmake Dadmehr0"""+c.GREEN+"""
+ /___/ .__/\_,_/_/_/_/ /___/_/_/_/___/"""+c.WHITE+"""V 1.6 Remaked Dadmehr0"""+c.GREEN+"""
     /_/""")
 
         # Sleep time for printing banner as successufly
         time.sleep(3)
 
+commands_help = ['-h','-help']
+def lunch():
+    def help():
+        print('''Commands\t\t\tUsage\n\nclear\t\t\tfor clear terminal\nexit\t\t\tfor exit script\nstart\t\t\tTo start the script and give information\n''')
+
+    while True:
+        terminal = input(" $ ")
+
+        if terminal in commands_help:
+            help()
+        elif terminal == 'clear':
+            banner()
+        elif terminal == 'exit':
+            exit()
+        elif terminal == 'start':
+
+            print(c.CYAN + "Please enter your target phonenumber")
+            phonenumber = input(c.WHITE + " $ ")
+
+            print(c.CYAN + "Please enter sms number")
+            sms_number = input(c.WHITE + " $ ")
+
+            sms_boomber(phonenumber, sms_number)
+        else:
+            print(c.RED+terminal+": Command not found")
+
+
 if __name__ == "__main__":
-    main.banner()
-    main.lunch()
+    banner()
+    lunch()
