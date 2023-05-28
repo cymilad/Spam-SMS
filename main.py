f0a16ab8c20270f80f09f94352fd1b46462e997f
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-28 03:34:21 +0330

    fix

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+# Dadmehr0 - Garfox
+import os
+import time
+
 from sms_boomber import sms_boomber
-from banenr import main_banner
 from colorama import Fore as c
 from setting import (
     INPUT_STRUCT
 )
+class main:
+    commands_help = ['-h','-help']
+    def lunch():
+        def help():
+            print('''Commands\t\t\tUsage\n\nclear\t\t\tfor clear terminal\nexit\t\t\tfor exit script\nstart\t\t\tTo start the script and give information\n''')
 
-def main():
-    main_banner()
+        while True:
+            terminal = input(" $ ")
 
-    print(c.CYAN + "Please enter your target phonenumber")
-    phonenumber = input(c.WHITE + " $ ")
+            if terminal in commands_help:
+                help()
+            elif == 'clear'
+                os.system('clear' or 'cls')
+            elif  == :
+        print(c.CYAN + "Please enter your target phonenumber")
+        phonenumber = input(c.WHITE + " $ ")
 
-    print(c.CYAN + "Please enter sms number")
-    sms_number = input(c.WHITE + " $ ")
+        print(c.CYAN + "Please enter sms number")
+        sms_number = input(c.WHITE + " $ ")
 
-    sms_boomber(phonenumber, sms_number)
+        sms_boomber(phonenumber, sms_number)
 
-main()
+    def banner():
+        os.system("clear" or "cls")
+
+        print(c.GREEN + """
+   ___ ___  ___ ___ _    ___ __ _  ___
+  (_-</ _ \/ _ `/  ' \  (_-</  ' \(_-< Help: -h,-help
+ /___/ .__/\_,_/_/_/_/ /___/_/_/_/___/"""+c.WHITE+"""V 1.6 Rmake Dadmehr0"""+c.GREEN+"""
+    /_/""")
+
+        # Sleep time for printing banner as successufly
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main.banner()
+    main.lunch()
--- setting.py
+++ setting.py
@@ -2,5 +2,7 @@ from colorama import Fore as c
 
 # cont varable
 URL = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
+
 INPUT_STRUCT = c.RED + " [" + c.WHITE + "~" + c.RED + "]"
+
 STRUCT = c.RED + " [" + c.WHITE + "!" + c.RED + "]"
